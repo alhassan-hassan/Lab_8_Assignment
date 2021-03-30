05292b7e455577c4f6e82f6fd4f8e305eb9a6fe2
Alhassan Hassan <66450402+alhassan-hassan@users.noreply.github.com>
2021-03-30 03:04:11 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,62 +1,92 @@
-track++;
-        for (int j = 0; j <= i; j++)
-        {
-            // A[i][j] = 10;
-            cout << A[i][j] << "\t";
-        }
-        cout << endl;
-    }
-}
+#include<iostream>
+#include<string>
+#include<cmath>
 
+using namespace std;
 
-void populateMatrix()
+int sizeL;
+int **A;
+int sizeLinear;
+int *B;
+int** multi;
+int reverseIndx(int I);
+int linearIndx(int i, int j);
+int createArray(int size);
+
+int main ()
 {
-    srand(time(NULL));
-    for (int i = 0 ; i < N; i++)
-    {
-        for (int j = 0; j < N; j++)
-        {
-            A[i][j] = rand()%1000;        
-        }
-    }
-}
-int LinearIndx(int i,int j)
-{
-    int index = 0;
-   
-    for (int m = 0; m <N; m++)
-    {
-        for ( int n = 0; n <= m; n++)
-        {
-            if(n == j && m == i){
-                return index;
+    // caliing function
+    createArray(5);
+    createArray(32,0,0,5);
+   createArray(128,4,3,5);
+   linearIndx(1,2);
+   reverseIndx(3);
+
+  }
+
+int createArray(int size) {
+    sizeL = size;
+
+    int A[size][size];
+
+    //size of unit array
+    int uniArraySize = round((size*size)/2) + 2;
+    int counter = 0;
+    // creating multidimensial array
+    int B[uniArraySize];
+    int C[size][size];
+
+    for (int i = 0; i < size; i ++) {
+        for (int j = 0; j < size; j ++) {
+            if (i >= j) {
+                A[i][j] = 1 + rand() %1000;
+                cout<<A[i][j]<<" ";
+                B[counter] = A[i][j];
+                counter++;
             }
-            index++;    
+        }
+        cout<<endl;
+    }
+
+    //print first 20 elements of multidimensional array
+    int track = 0;
+    for (int i = 0; i < 20; i ++) {
+        for (int j = 0; j <= i; j ++) {
+        if (track == 20){break;}
+            cout<<A[i][j]<< "   index ("<<i<<","<<j<<")"<<endl;
+            track++;
         }
     }
+
+    cout<<endl;
+
+    cout<<"THe first 20 elements of the unidimensional array is:"<<endl;
+    for (int i = 0; i < 20; i ++) {
+        cout<<B[i]<<" index:  "<<i<<endl;
+    }
+    cout<<endl<<endl;
+}
+
+
+int linearIndx(int i, int j){
+
+    int indexInUniDimen = ((i +1) * i)/2 +j;
+    cout<< "The index of ("<<i<<","<<j<<") is "<<indexInUniDimen<<" in the unidimensional array"<<endl<<endl;
     return 0;
 }
-void InverseIndx(int m, int &p,int &q)
-{
-    int index = 0;
-    // int i,j;
-    
-    for ( p = 0; p < N; p++){
-        for ( q = 0; q<=p; q++){
-            if(++index == m)
-                break;
-        }
-        if(index == m)
-                break;
-    }
 
+int reverseIndx(int I) {
+    ///THE CODE BELOW DOES THE WORK OF THE INVERSE FUNCTION
+
+    cout<<sizeof(B);
+    int indexMultiDimenI = (I % sizeL);
+    int indexMultiDimenJ = ceil(I/sizeL);
+    //cout<< "The index of at the "<<I<<"th position in the unidimena=sional array is ("<<indexMultiDimenI<<","<<indexMultiDimenJ<<") in the multidimensional array"<<endl;
+    return 0;
 }
 
-int ** createArray()
-{
-    int **a =(int**) malloc(N*sizeof(int*));
-    for (int i = 0; i < N; i++)
-        a[i] = (int*) malloc(N*sizeof(int));
-    return a;
 
-}
+
+
+
+
